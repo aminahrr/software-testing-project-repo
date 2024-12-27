3b62aed19ddd029bd7c0142b5165876161b632cc
aminahrr <132758347+aminahrr@users.noreply.github.com>
2024-12-27 01:43:28 +0100

    Update playwright.config.ts
    Basic setup, change later if its bad

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,1 +1,13 @@
 //config stuff
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  use: {
+    headless: true,
+  },
+  projects: [
+    { name: 'Chromium', use: { browserName: 'chromium' } },
+    { name: 'Firefox', use: { browserName: 'firefox' } },
+    { name: 'WebKit', use: { browserName: 'webkit' } },
+  ],
+});
