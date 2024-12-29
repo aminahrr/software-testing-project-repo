b34d07f272602d1dc7b66f013ca2dba3ab37f1e8
Hamza <hsmovich@gmail.com>
2024-12-29 01:56:18 +0100

    Removing the non-POM test; preparing the HomePage.ts file

--- page-objects/HomePage.ts
+++ page-objects/HomePage.ts
@@ -3,19 +3,16 @@ import { AbstractPage } from "./AbstractPage";
 
 export class HomePage extends AbstractPage{
 
+    // Instruction to which hyperlink we are pointing
 
-    readonly menu: Locator
-    readonly url: string
-
-
-    constructor(page: Page) {
-        super(page)
-        this.menu = page.locator('div[id="store.menu"]')
-        this.url = 'https://pandicapet.shop/'
+    public async goto(){
+        await this.page.goto('https://pandicapet.shop/')
     }
 
+    // Locators
 
-    public async navigateToHomePage() {
-        await this.page.goto(this.url)
-    }
+
+    // Actions
+
+    
 }
--- tests/smoke/smoke-loginLogOut.spec.ts
+++ tests/smoke/smoke-loginLogOut.spec.ts
@@ -2,17 +2,6 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../page-objects/LoginPage';
 import { AccountPage } from '../../page-objects/AccountPage';
 
-test('test', async ({ page }) => {
-  await page.goto('https://pandicapet.shop/');
-  await page.getByRole('link', { name: 'My Account' }).click();
-  await page.getByLabel('Korisničko ime ili email').click();
-  await page.getByLabel('Korisničko ime ili email').fill('omerkrtina@gmail.com');
-  await page.getByLabel('Šifra *').click();
-  await page.getByLabel('Šifra *').fill('qDfVm!WWmjiAyB*GELZ2kQkjn!@h9Qe9eK4DJvMapGBhGswZ6');
-  await page.getByRole('button', { name: 'Prijava' }).click();
-});
-
-
 test('POM Login Test', async ({page}) => {
   const loginPage = new LoginPage(page);
   const accountPage = new AccountPage(page);
