4bb4d0bfadd580197f9da14c9e10c4453576925b
Hamza <hsmovich@gmail.com>
2024-12-29 02:40:41 +0100

    reverting auth changes, realizing this was a shit idea lol

--- tests/smoke/smoke-loginLogOut.spec.ts
+++ tests/smoke/smoke-loginLogOut.spec.ts
@@ -14,7 +14,18 @@ test('POM Login Test', async ({page}) => {
   await loginPage.clickLoginButton();
 
   await accountPage.assertLoggedIn();
-
-
 })
 
+test('POM Logout Test', async ({page}) => {
+  const loginPage = new LoginPage(page);
+  const accountPage = new AccountPage(page);
+
+  await loginPage.goto();
+  await loginPage.clickEmailInputBox();
+  await loginPage.emailInputBox().fill('omerkrtina@gmail.com');
+  await loginPage.clickPasswordInputBox();
+  await loginPage.passwordInputBox().fill('qDfVm!WWmjiAyB*GELZ2kQkjn!@h9Qe9eK4DJvMapGBhGswZ6');
+  await loginPage.clickLoginButton();
+
+  await accountPage.assertLoggedIn();
+})
