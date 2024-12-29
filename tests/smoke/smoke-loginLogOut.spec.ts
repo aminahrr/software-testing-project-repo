dcf1a26f1ae550f6a4b313f07ff552447d2d3af8
Hamza <hsmovich@gmail.com>
2024-12-29 03:19:53 +0100

    Adding documentation

--- tests/smoke/smoke-loginLogOut.spec.ts
+++ tests/smoke/smoke-loginLogOut.spec.ts
@@ -6,12 +6,17 @@ test('POM Login/Logout Test', async ({page}) => {
   const loginPage = new LoginPage(page);
   const accountPage = new AccountPage(page);
 
-  await loginPage.goto();
+  await loginPage.goto(); // Goes to the homepage of the website
 
+  // The next line enters the login credentials and proceeds
   await loginPage.signIn('omerkrtina@gmail.com', 'qDfVm!WWmjiAyB*GELZ2kQkjn!@h9Qe9eK4DJvMapGBhGswZ6')
-  await accountPage.assertLoggedIn();
+  await page.waitForTimeout(3000); // Waits for 3000 milliseconds (GitHub test fix)
+  await accountPage.assertLoggedIn(); // Checks for the WordPress Account Panel
 
-  await accountPage.signOut();
-  await loginPage.assertLoggedOut()
+
+
+  await accountPage.signOut(); // Loging out
+  await page.waitForTimeout(3000); // Waits for 3000 milliseconds (GitHub test fix)
+  await loginPage.assertLoggedOut() // Check for the WordPress Login Panel
 })
 
