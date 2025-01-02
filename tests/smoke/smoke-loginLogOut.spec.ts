bc668d148b52a4e4953ad94a4d8324595bd28718
Hamza <hsmovich@gmail.com>
2025-01-02 17:03:34 +0100

    Dividing the login/logout test into two separate tests

--- tests/smoke/smoke-loginLogOut.spec.ts
+++ tests/smoke/smoke-loginLogOut.spec.ts
@@ -3,7 +3,7 @@ import { LoginPage } from '../../page-objects/LoginPage';
 import { AccountPage } from '../../page-objects/AccountPage';
 import { HomePage } from '../../page-objects/HomePage';
 
-test('POM Login/Logout Test', async ({page}) => {
+test('POM Login Test', async ({page}) => {
   const loginPage = new LoginPage(page);
   const accountPage = new AccountPage(page);
   const homePage = new HomePage(page);
@@ -14,6 +14,19 @@ test('POM Login/Logout Test', async ({page}) => {
   await loginPage.signIn('omerkrtina@gmail.com', 'qDfVm!WWmjiAyB*GELZ2kQkjn!@h9Qe9eK4DJvMapGBhGswZ6') // Entering the login credentials and proceeds
   await page.waitForTimeout(3000); // Waits for 3000 milliseconds (GitHub test fix)
   await accountPage.assertLoggedIn(); // Checks for the WordPress Account Panel
+}) 
+
+test('POM Logout Test', async ({page}) => {
+  const loginPage = new LoginPage(page);
+  const accountPage = new AccountPage(page);
+  const homePage = new HomePage(page);
+
+  await homePage.goto(); // Goes to the homepage of the website
+
+  // Login prerequisite
+  await loginPage.signIn('omerkrtina@gmail.com', 'qDfVm!WWmjiAyB*GELZ2kQkjn!@h9Qe9eK4DJvMapGBhGswZ6') // Entering the login credentials and proceeds
+  await page.waitForTimeout(3000); // Waits for 3000 milliseconds (GitHub test fix)
+  
 
 
   // Logout test
