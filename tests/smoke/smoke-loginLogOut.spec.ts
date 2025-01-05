af95f636fe41e6e644908bf6202f92a8ca59ec34
Hamza <hsmovich@gmail.com>
2025-01-05 03:43:16 +0100

    Speeding up the login/logout test ggwpv2

--- tests/smoke/smoke-loginLogOut.spec.ts
+++ tests/smoke/smoke-loginLogOut.spec.ts
@@ -3,34 +3,38 @@ import { LoginPage } from '../../page-objects/LoginPage';
 import { AccountPage } from '../../page-objects/AccountPage';
 import { HomePage } from '../../page-objects/HomePage';
 
-test('POM Login Test', async ({page}) => {
-  const loginPage = new LoginPage(page);
-  const accountPage = new AccountPage(page);
-  const homePage = new HomePage(page);
 
-  await homePage.goto(); // Goes to the homepage of the website
-
-  // Login test
-  await loginPage.signIn('omerkrtina@gmail.com', 'qDfVm!WWmjiAyB*GELZ2kQkjn!@h9Qe9eK4DJvMapGBhGswZ6') // Entering the login credentials and proceeds
-  await page.waitForTimeout(3000); // Waits for 3000 milliseconds (GitHub test fix)
-  await accountPage.assertLoggedIn(); // Checks for the WordPress Account Panel
-}) 
-
-test('POM Logout Test', async ({page}) => {
-  const loginPage = new LoginPage(page);
-  const accountPage = new AccountPage(page);
-  const homePage = new HomePage(page);
-
-  await homePage.goto(); // Goes to the homepage of the website
-
-  // Login prerequisite
-  await loginPage.signIn('omerkrtina@gmail.com', 'qDfVm!WWmjiAyB*GELZ2kQkjn!@h9Qe9eK4DJvMapGBhGswZ6') // Entering the login credentials and proceeds
-  await page.waitForTimeout(3000); // Waits for 3000 milliseconds (GitHub test fix)
+test.describe('Login & Logout Test', () => {
+  let homePage;
+  let accountPage;
+  let loginPage;
   
+  // before the test we navigate to the homepage
+  test.beforeEach(async ({page}) => {
+    homePage = new HomePage(page);
+    accountPage = new AccountPage(page);
+    loginPage = new LoginPage(page);
+    await homePage.goto();
+  }) 
 
+  // Testing the Login functionality
+  test('Login Test', async ({page}) => {
 
-  // Logout test
-  await accountPage.signOut(); // Loging out
-  await page.waitForTimeout(3000); // Waits for 3000 milliseconds (GitHub test fix)
-  await loginPage.assertLoggedOut() // Check for the WordPress Login Panel
-}) 
+  await loginPage.signIn('omerkrtina@gmail.com', 'qDfVm!WWmjiAyB*GELZ2kQkjn!@h9Qe9eK4DJvMapGBhGswZ6') // Entering the login credentials and proceeds
+  await accountPage.assertLoggedIn(); // Checks for the WordPress Account Panel
+
+  }) 
+
+  // Testing the Logout functionality
+  test('Logout test', async ({page}) => {
+
+    // Login prerequisite
+    await loginPage.signIn('omerkrtina@gmail.com', 'qDfVm!WWmjiAyB*GELZ2kQkjn!@h9Qe9eK4DJvMapGBhGswZ6') // Entering the login credentials and proceeds
+
+    // Logout test
+    await accountPage.signOut(); // Loging out
+    await loginPage.assertLoggedOut() // Check for the WordPress Login Panel
+
+  }) 
+
+})
